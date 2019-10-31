8600a89842a8ad922de632a2420fce243f4877ee
5ia17daloeffler <5ia17daloeffler>
2019-10-31 07:07:18 -0700

    Example

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,29 +1,63 @@
+const URL = 'http://localhost:8081';
+let entries = [];
 
+const dateAndTimeToDate = (dateString, timeString) => {
+    return new Date(`${dateString}T${timeString}`).toISOString();
+};
 
-function onCreate() {
-    var data = "{\"checkIn\":\"2014-01-01T19:28:56.782Z\",\"checkOut\":\"2014-01-01T23:28:56.782Z\"}";
+const createEntry = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const entry = {};
+    entry['checkIn'] = dateAndTimeToDate(formData.get('checkInDate'), formData.get('checkInTime'));
+    entry['checkOut'] = dateAndTimeToDate(formData.get('checkOutDate'), formData.get('checkOutTime'));
 
-    $.ajax({
-        type:"POST",
-        url:"http://localhost:8081/entries",
-        data: data,
-        dataType: "json",
-        contentType: "application/json"
+    fetch(`${URL}/entries`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(entry)
+    }).then((result) => {
+        result.json().then((entry) => {
+            entries.push(entry);
+    renderEntries();
+});
+});
+};
 
+const indexEntries = () => {
+    fetch(`${URL}/entries`, {
+        method: 'GET'
+    }).then((result) => {
+        result.json().then((result) => {
+            entries = result;
+    renderEntries();
+});
+});
+    renderEntries();
+};
 
-        /*
-        success: function (response) {
-            
-        }
-        */
-    });
+const createCell = (text) => {
+    const cell = document.createElement('td');
+    cell.innerText = text;
+    return cell;
+};
 
-}
+const renderEntries = () => {
+    const display = document.querySelector('#entryDisplay');
+    display.innerHTML = '';
+    entries.forEach((entry) => {
+        const row = document.createElement('tr');
+    row.appendChild(createCell(entry.id));
+    row.appendChild(createCell(new Date(entry.checkIn).toLocaleString()));
+    row.appendChild(createCell(new Date(entry.checkOut).toLocaleString()));
+    display.appendChild(row);
+});
+};
 
-function onShow() {
-    
-}
-
-function onDelete() {
-    
-}
+document.addEventListener('DOMContentLoaded', function(){
+    const createEntryForm = document.querySelector('#createEntryForm');
+    createEntryForm.addEventListener('submit', createEntry);
+    indexEntries();
+});
